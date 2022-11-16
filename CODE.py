f76ebc952017602d307cc4d65c42e67168dd0277
vkbbvcvnvvv <94930978+vkbbvcvnvvv@users.noreply.github.com>
2022-11-16 02:14:04 +0200

    Add files via upload

---
+++ CODE.py
@@ -0,0 +1,60 @@
+import os
+import time
+os.system ("clear")
+#My_Baner_From_DeadCod✗
+print ("This Script Made by MR | ANDR0ID ")
+print ("\033[1;31m __  __ ____    _      _    _   _ ____  ____   ___ ___ ____\033[4;30m")
+print ("\033[1;31m|  \/  |  _ \  | |    / \  | \ | |  _ \|  _ \ / _ \_ _|  _ \ \033[4;30m")
+print ("\033[1;31m| |\/| | |_) | | |   / _ \ |  \| | | | | |_) | | | | || | | |\033[4;30m")
+print ("\033[1;31m| |  | |  _ <  | |  / ___ \| |\  | |_| |  _ <| |_| | || |_| |\033[4;30m")
+print ("\033[1;31m|_|  |_|_| \_\ | | /_/   \_\_| \_|____/|_| \_\\___/___|____/\033[4;30m")
+print ("\033[4;30m                         |_|_ \033[4;30m")
+print ("                      ")
+print ("                      ")
+print ("                      ")
+#------------------------------------------------------------------
+#Comend_メ
+print ("[1] Install Root in Turmax \033[1;34m")
+time.sleep (0.3)
+print ("[2] Install Termux Desktop \033[1;34m")
+time.sleep (0.3)
+print ("[3] Install Kali in Termux \033[1;33m")
+time.sleep (0.3)
+print ("[4] install metasploit \033[1;33m")
+time.sleep(0.3)
+print ("[5] install nikto in termux\033[1;33m")
+time.sleep(0.3)
+print ("[6] install whois\033[1;33m")
+time.sleep(0.3)
+#--------------------------------------
+
+
+nun = input ("Chose Nuber : ")
+
+if nun == "1" :
+    os.system ("cd")
+    os.system ("pkg install proot -y")
+    os.system ("git clone https://github.com/MFDGaming/ubuntu-in-termux.git")
+if nun == "2" :
+     os.system ("apt update && apt upgrade -y")
+     os.system ("apt install git")
+     os.system ("git clone https://github.com/adi1090x/termux-desktop")
+if nun == "3" :
+    os.system ("pkg install wget")
+    os.system ("wget -O install-nethunter-termux https://offs.ec/2MceZWr")
+    os.system ("chmod +x install-nethunter-termux")
+    os.system ("./install-nethunter-termux")
+if nun == "4" :
+    os.system ("termux-open https://youtu.be/BYCa-RKwBck")
+    print ("https://mega.nz/file/vkV2DRxL#4aWrZeXEuVV0j8CsfwPg9CqAYol30bcYa7hBL9bRqCk")
+if nun == "5" :
+    os.system ("apt update && apt upgrade -y")
+    os.system ("apt install git")
+    os.system ("pkg install perl -y")
+    os.system ("cd")
+    os.system ("git clone https://github.com/sullo/nikto")
+    print ("@SimoTMT55BOT")
+if nun == "6" :
+     os.system ("apt-get install whois -y")
+     print ("----->whois DNS<------\033[1;34m")
+#MR | ANDR0ID
